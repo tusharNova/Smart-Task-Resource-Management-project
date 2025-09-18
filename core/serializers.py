49e3d17208f9eae114b2121e0aa29ad0abb4def4
tusharNova <tusharmankar02@gmail.com>
2025-09-18 14:44:38 +0530

    update serializers class

--- core/serializers.py
+++ core/serializers.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class UserBriefSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fileds = ('id', 'username', 'display_name', 'avatar')
+        fields = ('id', 'username', 'display_name', 'avatar')
 
 
 class AttachmentSerializer(serializers.ModelSerializer):
@@ -22,3 +22,39 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = ('id' , 'task' , 'author' , 'text' , 'created_at')
         read_only_fields = ('author' , 'created_at')
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    assignee = UserBriefSerializer(read_only = True)
+    assignee_id = serializers.PrimaryKeyRelatedField(
+        write_only = True , queryset = User.objects.all() , source = 'assignee', required = False ,allow_null = True
+    ) 
+
+    attachment = AttachmentSerializer(many = True , read_only = True)
+    comment = CommentSerializer(many = True , read_only = True)
+
+    class Meta:
+        model = Task
+        fields = (
+            'id' , 'project' , 'title' , 'descriptions' , 'assignee' ,'assignee_id' ,'status' , 'priority' , 'due_date' ,'attachment','comment' ,'watchers', 'created_at' , 'updated_at' 
+        )
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    owner = UserBriefSerializer(read_only = True)
+    owner_id = serializers.PrimaryKeyRelatedField(
+        write_only = True , queryset = User.objects.all() , source = 'owner', required = False ,allow_null = True
+    )
+    member = UserBriefSerializer(read_only = True , many = True)
+    member_id = serializers.PrimaryKeyRelatedField(
+        write_only = True , queryset = User.objects.all() , source = 'member', required = False ,allow_null = True
+    )
+
+    tasks = Task(many = True , read_only = True)
+
+    class Meta:
+        model = Project
+        fields = (
+            'id' , 'name' ,'descriptions' , 'owner' , 'owner_id' , 'member' , 'member_id' ,'task' ,  'created_at' , 'updated_at'
+        )
+        read_only_feilds = ('created_at' , 'updated_at' , 'owner' , 'members' , 'tasks')
